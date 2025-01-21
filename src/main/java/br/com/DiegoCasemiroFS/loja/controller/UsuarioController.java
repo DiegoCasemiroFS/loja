8b7ba844253a8c6466741db0b42e1e6471249131
Diego Casemiro <dcfs1998@gmail.com>
2025-01-21 15:50:18 -0300

    inserindo anotações e alterando método listaUsuarios

--- src/main/java/br/com/DiegoCasemiroFS/loja/controller/UsuarioController.java
+++ src/main/java/br/com/DiegoCasemiroFS/loja/controller/UsuarioController.java
@@ -6,6 +6,8 @@ import br.com.DiegoCasemiroFS.loja.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/usuario")
@@ -14,31 +16,31 @@ public class UsuarioController {
     private final UsuarioService usuarioService;
 
     @GetMapping("/procurarPorId/{id}")
-    public Usuario procurarPorId(Long id){
+    public Usuario procurarPorId(@PathVariable Long id){
         return usuarioService.procuraPorId(id);
     }
 
     @GetMapping("/procurarPorEmail/{email}")
-    public Usuario procurarPorEmail(String email){
+    public Usuario procurarPorEmail(@RequestBody String email){
         return usuarioService.procuraPorEmail(email);
     }
 
     @GetMapping("/listaUsuarios")
-    public void listaUsuarios(){
-        usuarioService.listaUsuarios();
+    public List<Usuario> listaUsuarios(){
+        return usuarioService.listaUsuarios();
     }
 
     @PostMapping("/cadastraUsuario")
-    public Usuario cadastraUsuario(Usuario usuario){
+    public Usuario cadastraUsuario(@RequestBody Usuario usuario){
         return usuarioService.cadastraUsuario(usuario);
     }
     @PutMapping("/atualizaUsuario/{id}")
-    public Usuario atualizaUsuario(Long id, UsuarioRequestDto usuarioRequestDto){
+    public Usuario atualizaUsuario(@PathVariable Long id, @RequestBody UsuarioRequestDto usuarioRequestDto){
         return usuarioService.atualizaUsuario(id, usuarioRequestDto);
     }
 
     @DeleteMapping("/deletaUsuario/{id}")
-    public void deletaUsuario(Long id){
+    public void deletaUsuario(@PathVariable Long id){
         usuarioService.deletaUsuario(id);
     }
 }
