e30f5483cae9fc124726f0e20d79e310fb0904b3
Diego Casemiro <dcfs1998@gmail.com>
2025-01-21 13:16:44 -0300

    inserindo anotação getter e setter

--- src/main/java/br/com/DiegoCasemiroFS/loja/entity/Pedido.java
+++ src/main/java/br/com/DiegoCasemiroFS/loja/entity/Pedido.java
@@ -3,11 +3,15 @@ package br.com.DiegoCasemiroFS.loja.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Pedido {
