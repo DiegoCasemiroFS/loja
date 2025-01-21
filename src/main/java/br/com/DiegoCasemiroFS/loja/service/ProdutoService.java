2d8785a377d03914c041978f9ed7665e2d995ec2
Diego Casemiro <dcfs1998@gmail.com>
2025-01-21 11:17:45 -0300

    criando ProdutoService

---
+++ src/main/java/br/com/DiegoCasemiroFS/loja/service/ProdutoService.java
@@ -0,0 +1,43 @@
+package br.com.DiegoCasemiroFS.loja.service;
+
+import br.com.DiegoCasemiroFS.loja.entity.Produto;
+import br.com.DiegoCasemiroFS.loja.entity.dtos.ProdutoRequestDto;
+import br.com.DiegoCasemiroFS.loja.exception.ProdutoNaoEncontradoException;
+import br.com.DiegoCasemiroFS.loja.repository.ProdutoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProdutoService {
+
+    private final ProdutoRepository produtoRepository;
+
+
+    public Produto procuraPorId(Long id){
+        return produtoRepository.findById(id).orElseThrow(ProdutoNaoEncontradoException::new);
+    }
+    public List<Produto> listaProdutos(){
+        return produtoRepository.findAll();
+    }
+
+    public Produto cadastraProduto(Produto produto){
+        return produtoRepository.save(produto);
+    }
+
+    public Produto atualizaProduto(Long id, ProdutoRequestDto produtoRequestDto){
+        Produto produtoAtualizado = produtoRepository.findById(id).orElseThrow(ProdutoNaoEncontradoException::new);
+        if(produtoAtualizado != null){
+            produtoAtualizado.setNome(produtoRequestDto.getNome());
+            produtoAtualizado.setPreco(produtoRequestDto.getPreco());
+            return produtoRepository.save(produtoAtualizado);
+        }
+        return null;
+    }
+
+    public void deletaProduto(Long id){
+        produtoRepository.deleteById(id);
+    }
+}
