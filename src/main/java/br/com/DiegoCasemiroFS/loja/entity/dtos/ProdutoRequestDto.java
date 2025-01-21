c437098bd90265db0a31f509b491e292762217b5
Diego Casemiro <dcfs1998@gmail.com>
2025-01-21 11:18:07 -0300

    inserindo anotação Getter

--- src/main/java/br/com/DiegoCasemiroFS/loja/entity/dtos/ProdutoRequestDto.java
+++ src/main/java/br/com/DiegoCasemiroFS/loja/entity/dtos/ProdutoRequestDto.java
@@ -1,10 +1,12 @@
 package br.com.DiegoCasemiroFS.loja.entity.dtos;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProdutoRequestDto {
